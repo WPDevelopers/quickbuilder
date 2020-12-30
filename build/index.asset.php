45db64d20b5dfe053b9570a3eb5c501f2f831995
Mahafuzur Rahaman <m.mahfuz.me@gmail.com>
2020-12-30 11:36:30 +0600

    Run Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3ea35c8e0f494f4fe491d1cf41ea1f70');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1d472acecfa207f92fd799d6798ab1f1');

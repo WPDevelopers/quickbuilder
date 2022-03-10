c2582c7b8f5eec7f541dc9f30eba604dc077ff01
Md. Alimuzzaman Alim <alim@ar.com.bd>
2022-03-10 13:40:48 +0600

    NPM build

--- dist/dev/quickbuilder.esm.js
+++ dist/dev/quickbuilder.esm.js
@@ -1028,28 +1028,38 @@ var useDefaults = function useDefaults(parentName, helpers, value, trigger) {
             helpers.setValue(eligibleKey, eligibleDefaultData ? eligibleDefaultData : eligibleDataToSet);
           }
         }
-      } else if (defaults !== null && defaults !== void 0 && defaults[value] && (isArray(defaults === null || defaults === void 0 ? void 0 : defaults[value]) || isObject(defaults === null || defaults === void 0 ? void 0 : defaults[value]))) {
-        for (var _i = 0, _Object$values = Object.values(defaults === null || defaults === void 0 ? void 0 : defaults[value]); _i < _Object$values.length; _i++) {
-          var eachKey = _Object$values[_i];
+      } else if (defaults !== null && defaults !== void 0 && defaults[value] && (isArray(defaults[value]) || isObject(defaults[value]))) {
+        for (var property in defaults[value]) {
+          var eachKey = defaults[value][property];
 
-          var _at = eachKey.indexOf("@"),
-              _colon = eachKey.indexOf(":");
+          if (eachKey && (isArray(eachKey) || isObject(eachKey))) {
+            var _eligibleDefaultData = helpers.getValueForDefault(property, parentName);
 
-          if (_at === 0 && _colon > 0) {
-            var _eligibleKey = eachKey.substr(1, _colon - 1);
-
-            var _eligibleDataToSet = eachKey.substr(_colon + 1);
-
-            if (eachKey.indexOf(".") > -1) {
-              _eligibleKey = _eligibleKey.split('.');
+            if (property != "" && eachKey != "") {
+              eachKey = eachKey === 'false' ? false : eachKey;
+              defaultsData[property] = _eligibleDefaultData ? _eligibleDefaultData : eachKey;
+              helpers.setValue(property, _eligibleDefaultData ? _eligibleDefaultData : eachKey);
             }
+          } else if (eachKey) {
+            var _at = eachKey.indexOf("@"),
+                _colon = eachKey.indexOf(":");
 
-            var _eligibleDefaultData = helpers.getValueForDefault(_eligibleKey, parentName);
+            if (_at === 0 && _colon > 0) {
+              var _eligibleKey = eachKey.substr(1, _colon - 1);
 
-            if (_eligibleKey != "" && _eligibleDataToSet != "") {
-              _eligibleDataToSet = _eligibleDataToSet === 'false' ? false : _eligibleDataToSet;
-              defaultsData[_eligibleKey] = _eligibleDefaultData ? _eligibleDefaultData : _eligibleDataToSet;
-              helpers.setValue(_eligibleKey, _eligibleDefaultData ? _eligibleDefaultData : _eligibleDataToSet);
+              var _eligibleDataToSet = eachKey.substr(_colon + 1);
+
+              if (eachKey.indexOf(".") > -1) {
+                _eligibleKey = _eligibleKey.split('.');
+              }
+
+              var _eligibleDefaultData2 = helpers.getValueForDefault(_eligibleKey, parentName);
+
+              if (_eligibleKey != "" && _eligibleDataToSet != "") {
+                _eligibleDataToSet = _eligibleDataToSet === 'false' ? false : _eligibleDataToSet;
+                defaultsData[_eligibleKey] = _eligibleDefaultData2 ? _eligibleDefaultData2 : _eligibleDataToSet;
+                helpers.setValue(_eligibleKey, _eligibleDefaultData2 ? _eligibleDefaultData2 : _eligibleDataToSet);
+              }
             }
           }
         }

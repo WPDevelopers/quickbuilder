124b542de2a236f38d7f12cfd51e9be6aa6fdecb
sapanmozammel <sapanmozammel@gmail.com>
2022-12-07 19:52:38 +0600

    .

--- src/fields/SelectAsync.tsx
+++ src/fields/SelectAsync.tsx
@@ -3,6 +3,7 @@ import AsyncSelect from "react-select/async";
 import { isArray, isObject, merge, wpFetch } from "../core/utils";
 import { withLabel, useBuilderContext } from "../core/hooks";
 import { when } from "../core";
+import parse from "html-react-parser";
 
 const SelectAsync = (props) => {
 	const builderContext = useBuilderContext();
@@ -17,16 +18,20 @@ const SelectAsync = (props) => {
 		inputValue: string,
 		callback: (options: []) => void
 	) => {
-		if(timeOutId){
+		if (timeOutId) {
 			clearTimeout(timeOutId);
 		}
 		let id = setTimeout(() => {
 			// AJAX
-			if (props?.ajax && when(props?.ajax?.rules, builderContext.values)) {
+			if (
+				props?.ajax &&
+				when(props?.ajax?.rules, builderContext.values)
+			) {
 				let data = { inputValue };
 				Object.keys(props?.ajax.data).map((singleData) => {
 					if (props?.ajax.data[singleData].indexOf("@") > -1) {
-						let eligibleKey = props?.ajax.data[singleData].substr(1);
+						let eligibleKey =
+							props?.ajax.data[singleData].substr(1);
 						data[singleData] = builderContext.values?.[eligibleKey];
 					} else {
 						data[singleData] = props?.ajax.data[singleData];
@@ -48,46 +53,72 @@ const SelectAsync = (props) => {
 				}
 			}
 		}, 300);
+		//@ts-ignore
 		setTimeOutId(id);
 	};
 
 	useEffect(() => {
-		if (!isArray(sOption) && isObject(sOption)) {
-			onChange({
-				target: {
-					type: "select",
-					name,
-					value: sOption,
-					options,
-					multiple,
-				},
-			});
-		}
-		if (isArray(sOption)) {
-			onChange({
-				target: {
-					type: "select",
-					name,
-					value: sOption,
-					options,
-					multiple,
-				},
-			});
-		}
+		onChange({
+			target: {
+				type: "select",
+				name,
+				value: sOption,
+				options,
+				multiple,
+			},
+		});
 	}, [sOption]);
 
 	return (
-		<div className="wprf-select-wrapper">
+		<div className="wprf-async-select-wrapper">
 			<AsyncSelect
 				cacheOptions
 				loadOptions={handleMenuOpen}
 				defaultOptions
 				isDisabled={props?.disable}
-				classNamePrefix="wprf-select"
+				classNamePrefix="wprf-async-select"
+				defaultMenuIsOpen={true}
+				menuIsOpen={true}
 				id={id}
 				name={name}
 				placeholder={placeholder}
+				formatOptionLabel={(option, meta) => {
+					if (meta?.inputValue?.length) {
+						if (
+							option?.name
+								.toLowerCase()
+								.includes(meta?.inputValue?.toLowerCase())
+						) {
+							let x = option?.name;
+							let regX = new RegExp(
+								`(${meta?.inputValue})`,
+								"gi"
+							);
+							let name = option?.name.replace(
+								regX,
+								"<strong style={font-weight: 900}>$1</strong>"
+							);
+							let address = option?.address.replace(
+								regX,
+								"<strong style={font-weight: 900}>$1</strong>"
+							);
+							return (
+								<>
+									{parse(name)}{" "}
+									<small>{parse(address)}</small>
+								</>
+							);
+						}
+					}
+					return (
+						<>
+							<b>{option?.name}</b>{" "}
+							<small>{option?.address}</small>
+						</>
+					);
+				}}
 				value={sOption}
+				isClearable={true}
 				isOptionDisabled={(option) => option?.disabled}
 				onChange={(option) => setSOption(option)} // option or options
 			/>

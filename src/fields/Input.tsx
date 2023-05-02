2bc7ebdf93527f97c83242cec8f0019c53dfb509
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-05-02 12:42:16 +0600

    Revert "Update Input.tsx"
    This reverts commit 3d4ce72fd64ee32372738c7a9ede12810804e435.

--- src/fields/Input.tsx
+++ src/fields/Input.tsx
@@ -1,14 +1,11 @@
 import { Button } from "@wordpress/components";
 import copy from "copy-to-clipboard";
-import React, { useCallback, useEffect, useState, useRef } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { withLabel } from '../core/hooks';
 import { validFieldProps } from '../core/utils';
-const Input = (props, ref?) => {
-	const type = props.type ? props.type : 'text';
-	const validProps = validFieldProps({...props, type}, ['is_pro', 'visible', 'trigger', 'copyOnClick', 'disable', 'parentIndex', 'context', 'badge', 'popup', 'tags']);
+const Input = (props) => {
+	const validProps = validFieldProps(props, ['is_pro', 'visible', 'trigger', 'copyOnClick', 'disable', 'parentIndex', 'context', 'badge', 'popup', 'tags']);
 	const handleChange = useCallback((event) => validProps.onChange(event, { popup: props?.popup, isPro: !!props.is_pro }), [validProps?.value]);
-	const localRef = useRef(null);
-	const inputRef = ref?.current ? ref : localRef;
 
 	if (validProps.type === 'checkbox') {
 		if (validProps?.name) {
@@ -29,7 +26,7 @@ const Input = (props, ref?) => {
 	}, [isCopied])
 
 
-
+	
 	if (!props.is_pro && props?.copyOnClick && props?.value) {
 		const copyMessage = props?.copyMessage || "Click To Copy!";
 		const copiedMessage = props?.copiedMessage || "Copied!";
@@ -41,7 +38,7 @@ const Input = (props, ref?) => {
 				},
 			});
 		};
-
+	
 		return <span className="wprf-clipboard-wrapper">
 			{React.createElement("input", { ...validProps, onChange: handleChange })}
 			<span className="wprf-clipboard-tooltip">
@@ -51,11 +48,55 @@ const Input = (props, ref?) => {
 		</span>;
 	}
 
+	if (props.tags) {
+		const tags: {label: string, value:string}[] = Object.values(props.tags);
+		const inputRef = React.useRef(null);
+
+		const clicked = (tag) => {
+			const input = inputRef.current;
+			const start = input?.selectionStart ?? 0;
+			const end = input?.selectionEnd ?? 0;
+			const before = props.value.slice(0, start);
+			const after = props.value.slice(end);
+			const newValue = before.concat(tag.value, after);
+
+			handleChange({
+				target: {
+					type: 'button',
+					name: props.name,
+					value: newValue
+				}
+			});
+		};
+
+		return <span className="wprf-tags-wrapper">
+		  {React.createElement("input", { ...validProps, onChange: handleChange, ref: inputRef })}
+		  {
+			tags.map((tag, index) => {
+			  return (
+				<React.Fragment key={index}>
+				  <span
+					className="button button-secondary"
+					title={tag.label}
+					onClick={(e) => clicked(tag)}
+				  >{tag.value}</span>
+				</React.Fragment>
+			  );
+			})
+		  }
+		</span>;
+
+	}
+
+
 	return React.createElement('input', {
-		...validProps, onChange: handleChange, ref: inputRef,
+		...validProps, onChange: handleChange
 	})
 }
 
+Input.defaultProps = {
+	type: 'text'
+}
 
-export const GenericInput = React.memo(React.forwardRef(Input));
+export const GenericInput = React.memo(Input);
 export default withLabel(React.memo(Input));

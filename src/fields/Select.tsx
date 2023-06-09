cad4f909a343c0a2bfbfc1cba1d80c7ace479818
Priyo Mukul <17237437+priyomukul@users.noreply.github.com>
2023-06-10 00:13:58 +0600

    Fixed: Select Field Issue (EMSOHEL UPDATE)

--- src/fields/Select.tsx
+++ src/fields/Select.tsx
@@ -1,121 +1,127 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import ReactSelect from "react-select";
-import { isArray, isObject, merge, wpFetch } from '../core/utils';
+import { isArray, isObject, merge, wpFetch, valueExists } from '../core/utils';
 import { withLabel, useOptions, useBuilderContext } from '../core/hooks';
 import { when } from '../core';
 
 const Select = (props) => {
-    const builderContext = useBuilderContext();
-    let { id, name, multiple, placeholder, search = false, onChange, parentIndex } = props;
-    const { options, selectedOption, setOptions, setData } = useOptions(props, 'options');
-    const [sOption, setSOption] = useState(null);
-    const [isLoading, setIsLoading] = useState(false)
-    const [isAjaxComplete, setIsAjaxComplete] = useState(false)
+	const builderContext = useBuilderContext();
+	let { id, name, multiple, placeholder, search = false, onChange, parentIndex } = props;
+	const { options, selectedOption, setOptions, setData } = useOptions(props, 'options');
+	const [sOption, setSOption] = useState(null);
+	const [isLoading, setIsLoading] = useState(false)
+	const [isAjaxComplete, setIsAjaxComplete] = useState(false)
 
-    const handleMenuOpen = () => {
-        // AJAX
-        if (props.ajax && (!props.ajax.rules || when(props.ajax.rules, builderContext.values))) {
-            setIsLoading(true);
-            let data = {};
-            Object.keys(props?.ajax.data).map(singleData => {
-                if (props?.ajax.data[singleData].indexOf('@') > -1) {
-                    let eligibleKey = props?.ajax.data[singleData].substr(1);
-                    data[singleData] = builderContext.values?.[eligibleKey]
-                } else {
-                    data[singleData] = props?.ajax.data[singleData]
-                }
-            })
-            if (!isAjaxComplete) {
-                return wpFetch({
-                    path: props?.ajax.api,
-                    data: data
-                }).then((response: any) => {
-                    setIsLoading(false);
-                    const arrayMerge = merge(props.options, response, 'value');
-                    builderContext.setFormField([...parentIndex, 'options'], arrayMerge)
-                    setData({
-                        options: arrayMerge,
-                        parentIndex: [...parentIndex, 'options']
-                    });
-                    // setIsAjaxComplete(true);
-                    return response;
-                })
-            }
-        }
-    }
-    const handleMenuClose = () => {
-        setIsLoading(false);
-    }
+	const handleMenuOpen = () => {
+		// AJAX
+		if (props.ajax && (!props.ajax.rules || when(props.ajax.rules, builderContext.values))) {
+			setIsLoading(true);
+			let data = {};
+			Object.keys(props?.ajax.data).map(singleData => {
+				if (props?.ajax.data[singleData].indexOf('@') > -1) {
+					let eligibleKey = props?.ajax.data[singleData].substr(1);
+					data[singleData] = builderContext.values?.[eligibleKey]
+				} else {
+					data[singleData] = props?.ajax.data[singleData]
+				}
+			})
+			if (!isAjaxComplete) {
+				return wpFetch({
+					path: props?.ajax.api,
+					data: data
+				}).then((response: any) => {
+					setIsLoading(false);
+					const arrayMerge = merge(props.options, response, 'value');
+					builderContext.setFormField([...parentIndex, 'options'], arrayMerge)
+					setData({
+						options: arrayMerge,
+						parentIndex: [...parentIndex, 'options']
+					});
+					// setIsAjaxComplete(true);
+					return response;
+				})
+			}
+		}
+	}
+	const handleMenuClose = () => {
+		setIsLoading(false);
+	}
 
-    useEffect(() => {
-        if (!isArray(sOption) && isObject(sOption)) {
-            onChange({
-                target: {
-                    type: 'select',
-                    name,
-                    value: sOption.value,
-                    options,
-                    multiple
-                },
-            });
-        }
-        if (isArray(sOption)) {
-            onChange({
-                target: {
-                    type: 'select',
-                    name,
-                    value: sOption.map(item => item.value),
-                    options,
-                    multiple
-                },
-            });
-        }
-    }, [sOption])
+	useEffect(() => {
+		if (!isArray(sOption) && isObject(sOption)) {
+			onChange({
+				target: {
+					type: 'select',
+					name,
+					value: sOption.value,
+					options,
+					multiple
+				},
+			});
+		}
+		if (isArray(sOption)) {
+			onChange({
+				target: {
+					type: 'select',
+					name,
+					value: sOption.map(item => item.value),
+					options,
+					multiple
+				},
+			});
+		}
+	}, [sOption])
 
-    useEffect(() => {
-        handleMenuOpen();
-    }, [])
+	useEffect(() => {
+		handleMenuOpen();
+	}, [])
 
-    useEffect(() => {
-        if (props?.menuOpen) {
-            handleMenuOpen();
-        }
-    }, [props?.menuOpen])
+	useEffect(() => {
+		if (props?.menuOpen) {
+			handleMenuOpen();
+		}
+	}, [props?.menuOpen])
 
-	const handleOptionChange = (option, value) => {
-		const values = option.map((item) => item.value);
-		let updatedOptions = option;
+	const handleOptionChange = useCallback((option) => {
+		if (isArray(option) && props?.filterValue?.length > 0) {
+			const origialValues = option;
+			let values = origialValues;
 
-		if ( value?.option?.value == 'all') {
-		  updatedOptions = option.filter((item) => item.value == 'all');
-		} else {
-		  // Remove "all" if any other option is selected
-		  updatedOptions = option.filter((item) => item.value !== 'all');
+			let filterValue = props?.filterValue ?? ['all'];
+			if (!isArray(filterValue)) {
+				filterValue = [filterValue];
+			}
+
+			if (origialValues?.length > 1 && valueExists(origialValues.map(item => item.value), filterValue)) {
+				values = origialValues.filter((item) => !filterValue.includes(item?.value));
+			}
+
+			option = values;
 		}
 
-		setSOption(updatedOptions);
-  	};
+		setSOption(option);
+	}, [name, id, parentIndex]);
 
-    return (
-        <div className="wprf-select-wrapper">
-            <ReactSelect
-                isDisabled={props?.disable}
-                classNamePrefix="wprf-select"
-                isSearchable={search ?? false}
-                id={id}
-                name={name}
-                isMulti={multiple ?? false}
-                placeholder={placeholder}
-                isLoading={isLoading}
-                options={options}
-                value={selectedOption}
-                onMenuOpen={handleMenuOpen}
-                onMenuClose={handleMenuClose}
-                isOptionDisabled={(option) => option?.disabled}
-                onChange={handleOptionChange} // option or options
-            />
-        </div>
-    )
+	return (
+		<div className="wprf-select-wrapper">
+			<ReactSelect
+				isDisabled={props?.disable}
+				classNamePrefix="wprf-select"
+				isSearchable={search ?? false}
+				id={id}
+				name={name}
+				isMulti={multiple ?? false}
+				placeholder={placeholder}
+				isLoading={isLoading}
+				options={options}
+				value={selectedOption}
+				onMenuOpen={handleMenuOpen}
+				onMenuClose={handleMenuClose}
+				isOptionDisabled={(option) => option?.disabled}
+				onChange={handleOptionChange} // option or options
+			/>
+		</div>
+	)
 }
 
 export default withLabel(Select);

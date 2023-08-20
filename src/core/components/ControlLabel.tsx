a2f05f6c682268a9f2cade27723341eca088963c
Mozammel Ali <sapanmozammel@gmail.com>
2023-08-20 19:07:35 +0600

    Update ControlLabel.tsx

--- src/core/components/ControlLabel.tsx
+++ src/core/components/ControlLabel.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
 const ControlLabel = (props) => {
-    const { id, label, badge, badgePosition, context, ...rest } = props;
-    if (!(label && label.length > 0)) {
-        return null;
-    }
+	const { id, label, badge, badgePosition, context, ...rest } = props;
+	if (!(label && label.length > 0)) {
+		return null;
+	}
 
-    /**
-     * Icon need to be fixed
-     * context?.icons?.[rest?.icon] through context
-     */
+	/**
+	 * Icon need to be fixed
+	 * context?.icons?.[rest?.icon] through context
+	 */
 
-    return (
-        <div className="wprf-control-label">
-            {badgePosition == 'left' && badge}
-            <label htmlFor={id}>{label}</label>
-            {rest?.link && <a rel="nofollow" target="_blank" href={rest.link}>{context?.icons?.link}</a>}
-            {badgePosition == 'right' && badge}
-        </div>
-    )
-}
+	return (
+		<div className="wprf-control-label">
+			{badgePosition == "left" && badge}
+			<label htmlFor={id}>{label}</label>
+			{rest?.label_subtitle && (
+				<p
+					className="wprf-label-subtitle"
+					dangerouslySetInnerHTML={{ __html: rest?.label_subtitle }}
+				></p>
+			)}
+			{rest?.link && (
+				<a rel="nofollow" target="_blank" href={rest.link}>
+					{context?.icons?.link}
+				</a>
+			)}
+			{badgePosition == "right" && badge}
+		</div>
+	);
+};
 export default ControlLabel;

a2e5216d5f774936d04a6b21d4e05774175b3f79
Mozammel Ali <sapanmozammel@gmail.com>
2023-08-23 11:12:04 +0600

    Update Section.tsx

--- src/fields/Section.tsx
+++ src/fields/Section.tsx
@@ -1,54 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import { Field } from '../fields';
-import { sortingFields } from '../core/utils';
-import { useBuilderContext } from '../core/hooks';
-import classNames from 'classnames';
+import classNames from "classnames";
+import React, { useEffect, useState } from "react";
+import { useBuilderContext } from "../core/hooks";
+import { sortingFields } from "../core/utils";
+import { Field } from "../fields";
 
 const Section = (props) => {
-    const builderContext = useBuilderContext();
-    const [isCollapse, setCollapse] = useState(props.collapsed ?? false);
-    const [fields, setFields] = useState([]);
+	const { searchable = false, searchPlaceholder = "Search..." } = props;
+	const builderContext = useBuilderContext();
+	const [isCollapse, setCollapse] = useState(props.collapsed ?? false);
+	const [fields, setFields] = useState([]);
 
-    useEffect(() => {
-        const newFields = sortingFields(props.fields);
-        /**
-         * FIXME: the line below the doc:
-         * Commented for Issue#11, Cycle 7
-         * Uncommented for Issue #38, Cycle 7
-         */
-        builderContext.setFormField([...props.parentIndex, 'fields'], newFields);
-        // builderContext.setFormField([...props.parentIndex, 'sorted'], true);
-        let allFields = newFields.map((item, index) => {
-            let parentIndex = [...props.parentIndex, 'fields', index];
-            return <Field key={item.name} {...item} parentIndex={parentIndex} />;
-        });
-        setFields(allFields);
-    }, [])
+	useEffect(() => {
+		const newFields = sortingFields(props.fields);
+		/**
+		 * FIXME: the line below the doc:
+		 * Commented for Issue#11, Cycle 7
+		 * Uncommented for Issue #38, Cycle 7
+		 */
+		builderContext.setFormField(
+			[...props.parentIndex, "fields"],
+			newFields
+		);
+		// builderContext.setFormField([...props.parentIndex, 'sorted'], true);
+		let allFields = newFields.map((item, index) => {
+			let parentIndex = [...props.parentIndex, "fields", index];
+			return (
+				<Field key={item.name} {...item} parentIndex={parentIndex} />
+			);
+		});
+		setFields(allFields);
+	}, []);
 
-    const componentClasses = classNames('wprf-control-section', props?.classes, props?.name, {
-        'wprf-section-collapsed': props?.collapsible && isCollapse,
-    })
+	const componentClasses = classNames(
+		"wprf-control-section",
+		props?.classes,
+		props?.name,
+		{
+			"wprf-section-collapsed": props?.collapsible && isCollapse,
+		}
+	);
 
-    return (
-        <div
-            id={props?.name}
-            className={componentClasses}>
-            {props.placeholder &&
-                <div className="wprf-section-title">
-                    <h4>{props.placeholder}</h4>
-                    {props.collapsible && (
-                        <button onClick={() => setCollapse(!isCollapse)}>
-                            {/* <Icon
+	return (
+		<div id={props?.name} className={componentClasses}>
+			{props.placeholder && (
+				<div className="wprf-section-title">
+					<h4>{props.placeholder}</h4>
+					{props.collapsible && (
+						<button onClick={() => setCollapse(!isCollapse)}>
+							{/* <Icon
                             icon={`arrow-${isCollapse ? "down" : "up"}-alt2`}
                         /> */}
-                            Icon
-                        </button>
-                    )}
-                </div>
-            }
-            <div className="wprf-section-fields">{fields}</div>
-        </div>
-    )
-}
+							Icon
+						</button>
+					)}
+				</div>
+			)}
+			{searchable ? (
+				<div className="wprf-section-fields">
+					<div className="wprf-section-search-form">
+						<form>
+							<input
+								type="search"
+								name=""
+								id=""
+								placeholder={searchPlaceholder}
+							/>
+							<button className="submit" hidden>
+								Search
+							</button>
+						</form>
+					</div>
+					<div className="wprf-section-search-results">{fields}</div>
+				</div>
+			) : (
+				<div className="wprf-section-fields">{fields}</div>
+			)}
+		</div>
+	);
+};
 
 export default React.memo(Section);

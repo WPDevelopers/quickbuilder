ed4f17d103dd8f56fac98053dd971ee1833e039b
Mozammel Ali <sapanmozammel@gmail.com>
2023-09-18 15:28:27 +0600

    Update Tab.tsx

--- src/fields/Tab.tsx
+++ src/fields/Tab.tsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import Menu from './tabs/Menu'
-import Content from './tabs/Content'
-import { TabConfig } from '../types/Tabs';
+import React, { useEffect, useState } from "react";
+import { TabConfig } from "../types/Tabs";
+import Content from "./tabs/Content";
+import Menu from "./tabs/Menu";
 // import { BuilderProvider } from '../core/hooks/useBuilderContext';
 // import useBuilder from '../core/hooks/useBuilder';
 
-import '../scss/index.scss';
-import { useBuilderContext } from '../core/hooks';
-import classNames from 'classnames';
-
+import classNames from "classnames";
+import { useBuilderContext } from "../core/hooks";
+import "../scss/index.scss";
 
 const Tab: React.FC<TabConfig> = (props) => {
-    // const builderContextState = useBuilder(props);
+	// const builderContextState = useBuilder(props);
 
-    const builderContext = useBuilderContext();
-    const [activeTab, setActiveTab] = useState(props?.value || props?.active);
+	const builderContext = useBuilderContext();
+	const [activeTab, setActiveTab] = useState(props?.value || props?.active);
+
+	const saveValue = props?.save ?? true;
 
 	const componentClasses = classNames(
 		"wp-react-form wprf-tabs-wrapper",
@@ -24,46 +25,46 @@ const Tab: React.FC<TabConfig> = (props) => {
 		}
 	);
 
-    useEffect(() => {
+	useEffect(() => {
 		const _activeTab = props.value ?? props.active;
 
-        if(_activeTab != activeTab){
-            setActiveTab(_activeTab);
-        }
-    }, [props?.value])
+		if (_activeTab != activeTab) {
+			setActiveTab(_activeTab);
+		}
+	}, [props?.value]);
 
-    useEffect(() => {
-        if(props.value !== activeTab){
-            props.onChange({
-                target: {
-                    type: 'button',
-                    name: props.name,
-                    value: activeTab
-                }
-            });
-        }
-    }, [activeTab])
+	useEffect(() => {
+		if (props.value !== activeTab && saveValue) {
+			props.onChange({
+				target: {
+					type: "button",
+					name: props.name,
+					value: activeTab,
+				},
+			});
+		}
+	}, [activeTab]);
 
-    return (
-        <div className={componentClasses}>
-            {/* <BuilderProvider value={builderContextState}> */}
-            <Menu
-                {...props}
-                active={activeTab}
-                setActive={(tabId) => setActiveTab(tabId)}
-                fields={props.fields}
-                context={builderContext}
-            />
-            <Content
-                {...props}
-                fields={props.fields}
-                active={activeTab}
-                setActive={(tabId) => setActiveTab(tabId)}
-                submit={props?.submit}
-            />
-            {/* </BuilderProvider> */}
-        </div>
-    )
-}
+	return (
+		<div className={componentClasses}>
+			{/* <BuilderProvider value={builderContextState}> */}
+			<Menu
+				{...props}
+				active={activeTab}
+				setActive={(tabId) => setActiveTab(tabId)}
+				fields={props.fields}
+				context={builderContext}
+			/>
+			<Content
+				{...props}
+				fields={props.fields}
+				active={activeTab}
+				setActive={(tabId) => setActiveTab(tabId)}
+				submit={props?.submit}
+			/>
+			{/* </BuilderProvider> */}
+		</div>
+	);
+};
 
 export default Tab;

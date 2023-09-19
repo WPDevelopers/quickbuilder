340091cba2b355274acacd7fa50a4653e1ca4e87
Mozammel Ali <sapanmozammel@gmail.com>
2023-09-19 15:27:53 +0600

    Update ColorPicker.tsx

--- src/fields/ColorPicker.tsx
+++ src/fields/ColorPicker.tsx
@@ -1,76 +1,86 @@
-import React, { useEffect, useRef, useState } from 'react'
 import { ColorPicker as WPColorPicker } from "@wordpress/components";
-import { __ } from '@wordpress/i18n';
-import { withLabel } from '../core/hooks';
-
+import { __ } from "@wordpress/i18n";
+import React, { useEffect, useRef, useState } from "react";
+import { withLabel } from "../core/hooks";
 
 const ColorPicker = (props) => {
-    const { value, name, id, onChange } = props;
-    const [showPicker, setShowPicker] = useState(false);
-    const [color, setColor] = useState(value || null)
-    const [defaultColor, setDefaultColor] = useState(null)
-    const closeRef = useRef(null);
+	const { value, name, id, onChange } = props;
+	const [showPicker, setShowPicker] = useState(false);
+	const [color, setColor] = useState(value || null);
+	const [defaultColor, setDefaultColor] = useState(value || null);
+	const closeRef = useRef(null);
 
-    useEffect(() => {
-        setDefaultColor(value);
-    }, [])
+	useEffect(() => {
+		if (value) setDefaultColor(value);
+		else setDefaultColor("#ffffff00");
+	}, []);
 
-    const handleCloseRef = (ref) => {
-        useEffect(() => {
-            const handleClickOutside = (ev) => {
-                if (ref.current && !ref.current.contains(ev.target)) {
-                    setShowPicker(false);
-                }
-            };
-            document.addEventListener("mousedown", handleClickOutside);
-            return () => {
-                document.removeEventListener("mousedown", handleClickOutside);
-            };
-        }, [ref]);
-    };
+	const handleCloseRef = (ref) => {
+		useEffect(() => {
+			const handleClickOutside = (ev) => {
+				if (ref.current && !ref.current.contains(ev.target)) {
+					setShowPicker(false);
+				}
+			};
+			document.addEventListener("mousedown", handleClickOutside);
+			return () => {
+				document.removeEventListener("mousedown", handleClickOutside);
+			};
+		}, [ref]);
+	};
 
-    useEffect(() => {
-        onChange({
-            target: {
-                type: 'colorpicker',
-                name,
-                value: color,
-            },
-        });
-    }, [color]);
+	useEffect(() => {
+		onChange({
+			target: {
+				type: "colorpicker",
+				name,
+				value: color,
+			},
+		});
+	}, [color]);
 
-    handleCloseRef(closeRef);
+	handleCloseRef(closeRef);
 
-    return (
-        <>
-            <div className="wprf-colorpicker-wrap" ref={closeRef}>
-                <input type="hidden" value={value} name={name} id={id} />
-                <span
-                    className="wprf-picker-display"
-                    style={{ backgroundColor: value, borderColor: value }}
-                    onClick={() => setShowPicker(!showPicker)}
-                ></span>
-                {showPicker && (
-                    <>
-                        <button
-                            className="wprf-colorpicker-reset"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                setColor(defaultColor);
-                                setShowPicker(false);
-                            }}
-                        >
-                            {__('Reset', 'notificationx')}
-                        </button>
-                        <WPColorPicker
-                            color={value}
-                            onChangeComplete={(event) => setColor(event.hex)}
-                        />
-                    </>
-                )}
-            </div>
-        </>
-    )
-}
+	return (
+		<>
+			<div className="wprf-colorpicker-wrap" ref={closeRef}>
+				<div
+					className="wprf-colorpicker-screen"
+					onClick={() => setShowPicker(!showPicker)}
+				>
+					<input type="hidden" value={value} name={name} id={id} />
+					<span className="wprf-picker-code">
+						{value || defaultColor}
+					</span>
+					<span
+						className="wprf-picker-display"
+						style={{ backgroundColor: value }}
+					></span>
+				</div>
+				{showPicker && (
+					<div className="wprf-colorpicker">
+						<WPColorPicker
+							color={value || defaultColor}
+							onChangeComplete={(event) => setColor(event.hex)}
+						/>
+						<div className="wprf-colorpicker-reset-wrap">
+							<button
+								className="wprf-colorpicker-reset"
+								onClick={(e) => {
+									e.preventDefault();
+									setColor(defaultColor);
+									setShowPicker(false);
+								}}
+							>
+								{props?.reset_text ??
+									__("Reset", "betterdocs")}
+							</button>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
 
 export default withLabel(ColorPicker);

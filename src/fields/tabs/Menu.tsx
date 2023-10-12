0592db97a9c558e9673232b992756c1d0e7bd056
Mozammel Ali <sapanmozammel@gmail.com>
2023-10-12 18:37:26 +0600

    add skip in steppedButton && set count on tab menu

--- src/fields/tabs/Menu.tsx
+++ src/fields/tabs/Menu.tsx
@@ -1,61 +1,77 @@
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 import classNames from "classnames";
 import React, { useEffect, useState } from "react";
 import { isObject, isString, isVisible } from "../../core/utils";
 import { Fields, TabMenuConfig } from "../../types/Tabs";
 
 const Menu: React.FC<TabMenuConfig> = (props) => {
-    if (props.fields === undefined) {
-        throw new Error(__("There are no tabs defined!", 'betterdocs'));
-    }
+	if (props.fields === undefined) {
+		throw new Error(__("There are no tabs defined!", "betterdocs"));
+	}
 
-    const { active, setActive, fields: tabs, context } = props;
+	const { active, setActive, fields: tabs, context } = props;
 
-    const [tabsFields, setTabsFields] = useState<Fields>([]);
+	const [tabsFields, setTabsFields] = useState<Fields>([]);
 
-    useEffect(() => {
+	useEffect(() => {
+		const filteredTabs = tabs.filter((tab) =>
+			isVisible(context?.values, tab)
+		);
+		setTabsFields(filteredTabs);
+	}, [tabs, context?.values?.source]);
 
-        const filteredTabs = tabs.filter(tab => isVisible(context?.values, tab));
-        setTabsFields(filteredTabs);
+	const componentClasses = classNames(
+		"wprf-tab-menu-wrapper",
+		props?.className,
+		{ "wprf-tab-menu-sidebar": props?.sidebar },
+		context?.values?.source
+	);
 
-    }, [tabs, context?.values?.source])
+	const currentTabIndex = tabsFields.findIndex(
+		(tab: any) => tab.id === active
+	);
 
-    const componentClasses = classNames(
-        "wprf-tab-menu-wrapper",
-        props?.className,
-        { "wprf-tab-menu-sidebar": props?.sidebar },
-        context?.values?.source
-    );
-
-    const currentTabIndex = tabsFields.findIndex((tab: any) => tab.id === active);
-
-    return (
-        <div className={componentClasses}>
-            <ul className="wprf-tab-nav">
-                {
-                    tabsFields.map((tab, index) => (
-                        <li
-                            className={classNames("wprf-tab-nav-item", {
-                                [`${tab.classes}`]: tab.classes,
-                                "wprf-active-nav": active === tab.id,
-                                "wprf-tab-complete": props?.completionTrack ? (index <= currentTabIndex) : false
-                            })}
-                            data-key={tab.id}
-                            key={tab.id}
-                            onClick={() => (props?.clickable ?? true) && setActive(tab.id)}
-                        >
-                            {
-                                tab?.icon && (
-                                    (isString(tab.icon) && !isObject(tab.icon)) ? <img src={tab.icon} alt={tab?.label} /> : (isObject(tab.icon) ? context?.icons?.[tab?.icon?.type]?.[tab?.icon?.name] : '')
-                                )
-                            }
-                            <span>{tab.label}</span>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div >
-    );
+	return (
+		<div className={componentClasses}>
+			<ul className="wprf-tab-nav">
+				{tabsFields.map((tab, index) => (
+					<li
+						className={classNames("wprf-tab-nav-item", {
+							[`${tab.classes}`]: tab.classes,
+							"wprf-active-nav": active === tab.id,
+							"wprf-tab-complete": props?.completionTrack
+								? index <= currentTabIndex
+								: false,
+						})}
+						data-key={tab.id}
+						key={tab.id}
+						onClick={() =>
+							(props?.clickable ?? true) && setActive(tab.id)
+						}
+					>
+						{props?.tab_number ? (
+							<span className="icon">
+								<span className="count">{index + 1}</span>
+							</span>
+						) : (
+							""
+						)}
+						{tab?.icon &&
+							(isString(tab.icon) && !isObject(tab.icon) ? (
+								<img src={tab.icon} alt={tab?.label} />
+							) : isObject(tab.icon) ? (
+								context?.icons?.[tab?.icon?.type]?.[
+									tab?.icon?.name
+								]
+							) : (
+								""
+							))}
+						<span>{tab.label}</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Menu;
--- src/fields/tabs/SteppedButton.tsx
+++ src/fields/tabs/SteppedButton.tsx
@@ -1,49 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from '@wordpress/components'
-import { useBuilderContext } from '../../core/hooks';
-import { Field } from '../../fields';
-import { SteppedButtonConfig } from '../../types/Tabs';
-import { isObject } from '../../core/utils';
+import { Button } from "@wordpress/components";
+import React, { useEffect, useState } from "react";
+import { useBuilderContext } from "../../core/hooks";
+import { Field } from "../../fields";
+import { SteppedButtonConfig } from "../../types/Tabs";
 
 const SteppedButton: React.FC<SteppedButtonConfig> = (props) => {
-    const [nextTab, setNextTab] = useState(undefined);
-    const [prevTab, setPrevTab] = useState(undefined)
-    const builderContext = useBuilderContext();
+	const [nextTab, setNextTab] = useState(undefined);
+	const [prevTab, setPrevTab] = useState(undefined);
+	const builderContext = useBuilderContext();
 
-    useEffect(() => {
-        const tabIds = props.fields.map(tab => tab.id);
-        const currentTabIndex = tabIds.findIndex(tab => tab === props.active);
-        if (currentTabIndex != -1) {
-            setPrevTab(tabIds[currentTabIndex - 1])
-        }
-        if (currentTabIndex <= tabIds.length) {
-            setNextTab(tabIds[currentTabIndex + 1])
-        }
-    }, [props.active, props.fields])
+	useEffect(() => {
+		const tabIds = props.fields.map((tab) => tab.id);
+		const currentTabIndex = tabIds.findIndex((tab) => tab === props.active);
+		if (currentTabIndex != -1) {
+			setPrevTab(tabIds[currentTabIndex - 1]);
+		}
+		if (currentTabIndex <= tabIds.length) {
+			setNextTab(tabIds[currentTabIndex + 1]);
+		}
+	}, [props.active, props.fields]);
 
-    return (
-        <div className="wprf-stepped-button">
-            {
-                props.config.buttons && Object.keys(props.config.buttons).map((button, index) => {
-                    return <React.Fragment key={`button_${button}_${index}`}>
-                        {
-                            ((button === 'next' && nextTab !== undefined) || (button === 'prev' && prevTab !== undefined)) &&
-                            <Button
-                                className={`wprf-btn wprf-step-btn-${button}`}
-                                onClick={() => props.setActive(button === 'next' ? nextTab : prevTab)}
-                            >
-                                {props.config.buttons?.[button]}
-                            </Button>
-                        }
-                        {
-                            nextTab == undefined && props.config.buttons?.[button]?.type && <Field {...props.config.buttons?.[button]} />
-                        }
-                    </React.Fragment>;
-                })
-            }
-        </div>
-    )
-}
-
+	return (
+		<div className="wprf-stepped-button">
+			{props.config.buttons &&
+				Object.keys(props.config.buttons).map((button, index) => {
+					return (
+						<React.Fragment key={`button_${button}_${index}`}>
+							{button === "skip" && nextTab !== undefined && (
+								<Button
+									className={`wprf-btn wprf-step-btn-${button}`}
+									onClick={() => props.setActive(nextTab)}
+								>
+									{props.config.buttons?.[button]}
+								</Button>
+							)}
+							{((button === "next" && nextTab !== undefined) ||
+								(button === "prev" &&
+									prevTab !== undefined)) && (
+								<Button
+									className={`wprf-btn wprf-step-btn-${button}`}
+									onClick={() =>
+										props.setActive(
+											button === "next"
+												? nextTab
+												: prevTab
+										)
+									}
+								>
+									{props.config.buttons?.[button]}
+								</Button>
+							)}
+							{nextTab == undefined &&
+								props.config.buttons?.[button]?.type && (
+									<Field
+										{...props.config.buttons?.[button]}
+									/>
+								)}
+						</React.Fragment>
+					);
+				})}
+		</div>
+	);
+};
 
 export default React.memo(SteppedButton);
